01bc1728b518c03cfb927cb49e1aebf2c7615327
Aniruddh M <animaruthesh@gmail.com>
2020-08-26 23:20:36 +0530

    Bubble Sort Working

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
 var InsertArrayClicked = false;
 var ArrayElements = [];
+const HighlightColor = "orange"
+const DefaultColor = "#333333"
+const mediumSpeed = 100
+const fastSpeed = 10
 
-function BubbleSort(Numbers){
+function createNumberBars(ArrayElements){
+    var VisualArea = document.getElementById("SortingArea")
+    console.log(screen.availWidth)
+    var WidthOfBars = screen.availWidth/(ArrayElements.length)
+    VisualArea.innerHTML = ''
+    var a= 0;
+    for(var i=0; i<ArrayElements.length; i++){
+        var Element = document.createElement("span");
+        Element.style.width = (WidthOfBars*3/4).toString() + "px"
+        Element.style.height = (ArrayElements[i]*2).toString() + "px"
+        Element.style.backgroundColor = "#333333"
+        Element.style.marginRight = (WidthOfBars/5).toString() + "px"
+        Element.style.display = "inline-block"
+        VisualArea.appendChild(Element)
+    }
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+function SetBarsColor(index1, index2, color){
+    var AllBars = document.getElementById("SortingArea").childNodes
+    var Bar1 = AllBars[index1]
+    var Bar2 = AllBars[index2]
+    Bar1.style.backgroundColor = color
+    Bar2.style.backgroundColor = color
+}
+
+function Swapbars(index1, index2){
+    var temp
+    var AllBars = document.getElementById("SortingArea").childNodes
+    var Bar1 = AllBars[index1]
+    var Bar2 = AllBars[index2]
+    temp = Bar2.style.height
+    Bar2.style.height = Bar1.style.height
+    Bar1.style.height = temp
+}
+
+function SwapNumbers(Array, index1, index2){
+    var temp;
+    temp = Array[index1]
+    Array[index1] = Array[index2]
+    Array[index2] = temp
+}
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function BubbleSort(Numbers){
+    //50 60 20 30 10 50 60 70 200
     for(var i=0; i<Numbers.length; i++){
+        var swapped = false
         for(var j=0; j<Numbers.length-i-1; j++){
             if( Numbers[j] > Numbers[j+1] ){
-                var temp;
-                temp = Numbers[j]
-                Numbers[j] = Numbers[j+1]
-                Numbers[j+1] = temp
+                setTimeout(SetBarsColor(j, j+1, HighlightColor))
+                await sleep(fastSpeed)
+                Swapbars(j, j+1)
+                SetBarsColor(j, j+1, DefaultColor)
+                SwapNumbers(Numbers, j, j+1)
+                swapped = true
             }
         }
+        if(!swapped){
+            break
+        }
     }
+    console.log(Numbers)
 }
 
 function OnClickInsertAnArray(){
@@ -47,7 +106,7 @@ function OnClickInputSubmitButton(){
     if( Numbers.length != 0){
         for(var i=0; i< Numbers.length; i++){
             if( isNaN(Numbers[i]) == false && Numbers[i] != "" ){
-                ArrayElements.push(Numbers[i])
+                ArrayElements.push(parseInt(Numbers[i]))
             }
         }
         var SortingChoice = document.getElementById('SortingAlgos').value;
@@ -71,22 +130,4 @@ function OnClickInputSubmitButton(){
             console.log(SortingChoice)
         }
     }
-}
-
-function createNumberBars(ArrayElements){
-    var VisualArea = document.getElementById("SortingArea")
-    console.log(screen.availWidth)
-    var WidthOfBars = screen.availWidth/(ArrayElements.length)
-    VisualArea.innerHTML = ''
-    var a= 0;
-    for(var i=0; i<ArrayElements.length; i++){
-        var Element = document.createElement("span");
-        Element.style.width = (WidthOfBars*3/4).toString() + "px"
-        Element.style.height = (ArrayElements[i]*2).toString() + "px"
-        Element.style.backgroundColor = "#333333"
-        Element.style.marginRight = (WidthOfBars/5).toString() + "px"
-        Element.style.display = "inline-block"
-        VisualArea.appendChild(Element)
-    }
-    window.scrollTo(0,document.body.scrollHeight);
 }
